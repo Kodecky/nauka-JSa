93f7038c56a4af5d10d195a3f7031dc1d281927b
Damian Pawela <paweladamian@gmail.com>
2017-12-29 14:36:20 +0100

    Add triangleArea

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 //scripts.js
-var name = prompt('enter your name');
-/*alert('Hello, '+ name);*/     //example comment
-console.log('Hello, '+ name);
+/*var name = prompt('enter your name');
+alert('Hello, '+ name);
+console.log('Hello, '+ name);*/
+
+var a = prompt('please enter value of base "a" '),
+    h = prompt('please, enter value of height "h" '),
+    triangleArea = a * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+        
